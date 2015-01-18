eb139a25d90d69be03d36c6ee323b0f123dc916f
William Mora <wilmor24@gmail.com>
2015-01-18 17:35:46 -0300

    Google Fitness boilerplate code

--- app/src/main/java/com/nispok/fitcoach/activities/WelcomeActivity.java
+++ app/src/main/java/com/nispok/fitcoach/activities/WelcomeActivity.java
@@ -6,7 +6,7 @@ import com.nispok.fitcoach.R;
 import com.nispok.fitcoach.fragments.WelcomeFragment;
 
 
-public class WelcomeActivity extends ToolbarActivity {
+public class WelcomeActivity extends GoogleFitnessClientActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
