e81b36706da8ab8aff3a8861703cc64f43934c1d
William Mora <wilmor24@gmail.com>
2015-01-19 00:02:02 -0300

    refactor

--- app/src/main/java/com/nispok/fitcoach/fragments/GoogleFitGoalSetupFragment.java
+++ app/src/main/java/com/nispok/fitcoach/fragments/GoogleFitGoalSetupFragment.java
@@ -13,7 +13,6 @@ public class GoogleFitGoalSetupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_google_fit_goal_setup, container, false);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_google_fit_goal_setup, container, false);
     }
 }
