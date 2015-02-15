19b59514651c1b9790455ed169fb7933940308c3
William Mora <wilmor24@gmail.com>
2015-02-15 19:48:24 -0300

    DBFlow integration

--- app/src/main/java/com/nispok/fitcoach/FitCoach.java
+++ app/src/main/java/com/nispok/fitcoach/FitCoach.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.nispok.fitcoach.services.GoalService;
 import com.crashlytics.android.Crashlytics;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 import io.fabric.sdk.android.Fabric;
 
 public class FitCoach extends Application {
@@ -14,8 +16,15 @@ public class FitCoach extends Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+        FlowManager.init(this);
         application = this;
         GoalService.getInstance().createWaterGoal();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        FlowManager.destroy();
     }
 
     public static Application getApplication() {
