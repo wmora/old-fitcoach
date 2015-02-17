b4e63e00ec683d3509bcf1ba21ae74216b27b9bc
William Mora <wilmor24@gmail.com>
2015-02-17 20:59:53 -0300

    request codes are back! add_goal rc added

--- app/src/main/java/com/nispok/fitcoach/intents/FitCoachIntent.java
+++ app/src/main/java/com/nispok/fitcoach/intents/FitCoachIntent.java
@@ -2,6 +2,10 @@ package com.nispok.fitcoach.intents;
 
 public class FitCoachIntent {
 
+    public static class RequestCode {
+        public static final int ADD_GOAL = 10000000;
+    }
+
     public static class Extra {
         public static final String GOAL_ID = "GOAL_ID";
     }
--- app/src/test/java/com/nispok/fitcoach/intents/FitCoachIntentTest.java
+++ app/src/test/java/com/nispok/fitcoach/intents/FitCoachIntentTest.java
@@ -7,6 +7,11 @@ import static junit.framework.Assert.assertEquals;
 public class FitCoachIntentTest {
 
     @Test
+    public void testWaterReminderRequestCodeShouldBe10000000() {
+        assertEquals(FitCoachIntent.RequestCode.ADD_GOAL, 10000000);
+    }
+
+    @Test
     public void testGoalIdExtraShouldBeGOAL_ID() {
         assertEquals(FitCoachIntent.Extra.GOAL_ID, "GOAL_ID");
     }
